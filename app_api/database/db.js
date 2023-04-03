8e4a3da06a83d8ded12803858719acf5f93660ab
Cristian Chavez <jayden9260@gmail.com>
2023-04-02 17:56:02 -0700

    Create new application API

--- app_api/database/db.js
+++ app_api/database/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const host = process.env.DB_HOST || '127.0.0.1'
-const dbURI = 'mongodb://localhost:27017/travlr';
+const dbURI = `mongodb://${host}/travlr`;
 const readLine = require('readLine');
 
 mongoose.set('useUnifiedTopology', true);
@@ -50,4 +50,5 @@ process.on('SIGTERM', () => {
   });
 });
 
+connect();
 require('./models/travlr');
